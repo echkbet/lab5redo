2a9a0794dcdc3a709687de004478afd915373b2e
echkbet <hilarys_skillery@yahoo.com>
2017-09-26 16:20:37 +0000

    LAB6

---
+++ LAB6/database.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$db = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+} 
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $db->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$db->close();
+?>
+Run example »
